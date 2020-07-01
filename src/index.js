92e0c2c103a15215b5b3d6309b2174b397071bf7
Nicolas <nicolasdellazzeri@gmail.com>
2020-07-01 11:56:31 -0300

    Reutilização de alteração

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import '../node_modules/regenerator-runtime/runtime.js'
 import './scss/index.scss';
 import './/../node_modules/toastr/build/toastr.css';
 import routes from './routes';
-// import App from './app';
+import App from './app';
 
-// let contentDiv = document.getElementById('content');
-// contentDiv.innerHTML = routes[window.location.pathname];
+let contentDiv = document.getElementById('content');
+contentDiv.innerHTML = routes[window.location.pathname];
 
-// window.onpopstate = () => {
-//   contentDiv.innerHTML = routes[window.location.pathname];
-// }
+window.onpopstate = () => {
+  contentDiv.innerHTML = routes[window.location.pathname];
+}
 
-// window.onload = App;
+window.onload = App;
